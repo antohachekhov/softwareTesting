d122a3014d26777a68e75eca41ef61b338b98ee6
Ефименко Александра Сергеевна <efimenko.2018@stud.nstu.ru>
2023-09-11 21:58:54 +0700

    Разработаны функции лр1.

---
+++ ЛР №1. Модульное тестирование программ на языке С# средствами Visual Studio/ConsoleApp1/ConsoleApp1/Array.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Array
+    {
+        // 1 Функция получает два одномерных массива.В массиве int[] ind хранятся индексы компонентов массива v.
+        // Найдите произведение ненулевых компонентов массива v, индексы которых хранятся в массиве ind.
+        public static double calcMultElemsByInd(double[] v, int[] ind)
+        {
+            double mult = 1.0;
+            for(int i = 0; i < ind.Length; i++)
+            {
+                int iv = ind[i];
+                if (v[iv] != 0)
+                {
+                    mult *= v[iv];
+                }
+            }
+            return mult;
+        }
+
+
+        // 2 Функция получает одномерный массив целых переменных.Вычисляет и возвращает минимальный по значению элемент этого массива и
+        // номер его индекса.
+        public static int[] findMinElemWithIndex(int[] arr)
+        {
+            int min = arr[0];
+            int indexOfMin = 0;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    indexOfMin = i;
+                }
+            }
+
+            int[] result = {min, indexOfMin};
+            return result;
+        }
+
+        // 3 Функция получает одномерный массив вещественных переменных. В полученном массиве функция переставляет значения компонентов
+        // массива в обратном порядке.
+        public static void reverseArray(ref double[] arr)
+        {
+            double[] revArr = new double[arr.Length];
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                revArr[i] = arr[arr.Length - i - 1];
+            }
+            arr = revArr;
+        }
+    }
+}
--- ЛР №1. Модульное тестирование программ на языке С# средствами Visual Studio/ConsoleApp1/ConsoleApp1/Program.cs
+++ ЛР №1. Модульное тестирование программ на языке С# средствами Visual Studio/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ind = { 1, 2, 3 };
+            double[] v = { 1.0, 2.0, 3.0, 4.0 };
+            Console.WriteLine("Произведение: " + Array.calcMultElemsByInd(v, ind));
+
+            int[] arr2 = { -20, 10000, 5, 7, -100000 };
+            int[] res2 = Array.findMinElemWithIndex(arr2);
+            Console.WriteLine("Минимальный элемент = " + Convert.ToString(res2[0]) + " и его индекс = " + Convert.ToString(res2[1]));
+
+            double[] check3 = { 1.24, 0.5, 4 };
+            Array.reverseArray(ref check3);
+            Console.Write("check3 reverse = " + string.Join(", ", check3));
+        }
+    }
+
+}
