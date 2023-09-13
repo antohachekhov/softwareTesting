7a624116c86d5ded990fe01ffef21ed7b3d16101
Ефименко Александра Сергеевна <efimenko.2018@stud.nstu.ru>
2023-09-13 22:13:51 +0700

    Добавлен проект тестов

---
+++ ЛР №1. Модульное тестирование программ на языке С# средствами Visual Studio/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
