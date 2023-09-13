f5d1ee5928e3c2feb02e26fd1772ae8341ca501e
antohachekhov <bortnikovnik@gmail.com>
2023-09-13 23:21:04 +0700

    Создан проект для тестирования

---
+++ ЛР №1. Модульное тестирование программ на языке С# средствами Visual Studio/ConsoleApp1/UnitTestProject_1/ArrayTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Program1;
+
+namespace UnitTestProject_1
+{
+    [TestClass]
+    public class ArrayTest
+    {
+
+        [TestMethod]
+        public void calcMultElemsByInd_Test_C0()
+        {
+            // Исходные массивы.
+            double[] v = new double[] { 1 };
+            int[] ind = new int[] { 0 };
+
+            int expected = 1; // Ожидаемое значение.
+
+            var result = ArrayClass.calcMultElemsByInd(v, ind);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void findMinElemWithIndex_Test_C0()
+        {
+            // Исходные массивы.
+            int[] x = new int[] { 0, 1 };
+
+            int[] expected = new int[] { 0, 0 }; // Ожидаемое значение.
+
+            var result = ArrayClass.findMinElemWithIndex(x);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void reverseArray_Test_C0()
+        {
+            double[] result = new double[] { 1.0, 2.0 };//Исходный массив.
+            double[] expected = new double[] { 2.0, 1.0 };//Ожидаемое значение.
+
+            ArrayClass.reverseArray(ref result);
+
+            CollectionAssert.AreEqual(expected, result);//Оракул.
+        }
+    }
+}
---
+++ ЛР №1. Модульное тестирование программ на языке С# средствами Visual Studio/ConsoleApp1/UnitTestProject_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject_1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d78dc94-f46d-4339-8307-8c2bfd87c872")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
