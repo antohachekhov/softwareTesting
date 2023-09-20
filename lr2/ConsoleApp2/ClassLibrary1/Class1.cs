a903d632d560ab6bad0341e53a222b50dda73526
Ефименко Александра Сергеевна <efimenko.2018@stud.nstu.ru>
2023-09-20 18:52:22 +0700

    Завершена 2лр

---
+++ lr2/ConsoleApp2/ClassLibrary1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        // Поиск минимума из трёх чисел
+        public static double findMin(double a, double b, double c)
+        {
+            double min;
+            if (a < b)
+            {
+                min = a;
+                if (c < min)
+                    min = c;
+            }
+            else
+            {
+                min = b;
+                if (c < min)
+                    min = c;
+            }
+            return min;
+        }
+
+        // Функция получает двумерный массив вещественных переменных A.
+        // Отыскивает и возвращает сумму значений компонентов массива, у которых сумма значений индексов – чётная.
+        public static double findSumElements(double[,] A)
+        {
+            double sum = 0;
+            for (int i = 0;
+                i < A.GetLength(0);
+                i++)
+                for (int j = 0;
+                j < A.GetLength(1);
+                j++)
+                {
+                    if ((i + j) % 2 == 0)
+                        sum += A[i,j];
+                }
+
+            return sum;
+        }
+
+        // Функция получает двумерный массив вещественных переменных A. 
+        // Отыскивает и возвращает максимальное значение компонентов массива, лежащих на и ниже главной диагонали
+        public static double maxElem(double[,] A)
+        {
+            double maxA = A[0,0];
+            for (int i = 0; 
+                i < A.GetLength(0);
+                i++)
+            {
+                for (int j = 0; 
+                    j <= i; 
+                    j++)
+                {
+                    if (A[i,j] > maxA)
+                        maxA = A[i,j];
+                }
+            }
+            return maxA;
+        }
+    }
+}
---
+++ lr2/ConsoleApp2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void minElemCAB_Test_C1()
+        {
+            //arrange
+            double a = 1, b = 2, c = 0;
+            int expected = 0;
+
+            // act
+            var result = Class1.findMin(a, b, c);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void minElemABC_Test_C1()
+        {
+            //arrange
+            double a = 1, b = 2, c = 3;
+            int expected = 1; 
+
+            // act
+            var result = Class1.findMin(a, b, c);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void minElemCBA_Test_C1()
+        {
+            //arrange
+            double a = 2, b = 1, c = 0;
+            int expected = 0;
+
+            // act
+            var result = Class1.findMin(a, b, c);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void minElemBAC_Test_C1()
+        {
+            //arrange
+            double a = 2, b = 1, c = 3;
+            int expected = 1;
+
+            // act
+            var result = Class1.findMin(a, b, c);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void findSumElements_Test_C1()
+        {
+            //arrange
+            double[,] A = { { 1, 2 } };
+            int expected = 1; 
+
+            // act
+            var result = Class1.findSumElements(A);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void maxElem_Test_C1()
+        {
+            // arrange
+            double[,] A = { { 1, 2 }, { 3, 4}};
+
+            int expected = 4; 
+
+            // act
+            var result = Class1.maxElem(A);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
